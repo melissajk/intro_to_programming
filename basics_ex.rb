c4a9dd22c34fe4a43e3ba8e9202894eed4ae9f73
Melissa Kaspshak <mj2tiger@gmail.com>
2016-09-14 16:25:38 +0000

    changed directories for programming exercies

---
+++ basics_ex.rb
@@ -0,0 +1,35 @@
+# Exercise 1 
+# puts 'Melissa ' + 'Kaspshak'
+
+# Exercise 2
+# puts 4321 / 1000
+# puts 4321 / 100 % 10
+# puts 4321 % 100 / 10
+# puts 4321 % 10
+
+# Exercise 3 
+# films = {:film_a => 1975, :film_b => 2004, :film_c => 2013, :film_d => 2001, :film_e => 1981}
+# puts films[:film_a], films[:film_b], films[:film_c], films[:film_d], films[:film_e]
+
+## here's another way to do it!
+# puts films.values
+
+# Exercise 4
+# films = [1975, 2004, 2013, 2001, 1981]
+# puts films[0..4]
+
+# Exercise 5
+# factorial_5 = 5 * 4 * 3 * 2 * 1
+# factorial_6 = 6 * factorial_5
+# factorial_7 = 7 * factorial_6
+# factorial_8 = 8 * factorial_7
+# puts factorial_5, factorial_6, factorial_7, factorial_8
+
+# Exercise 6
+# puts 5.0**2
+# puts 105.1**2
+# puts 48.52**2
+
+# Exercise 7 
+# It's a syntax error that's telling you that you closed a block with a
+#   parenthesis ')' instead of a bracket '}'. 
