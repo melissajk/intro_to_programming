23e7399fa2a2b7d2927cc68a411a0a1d62644e32
Melissa Kaspshak <mj2tiger@gmail.com>
2016-09-15 16:33:29 +0000

    Exercises from 'Variables' section

---
+++ var_exercises.rb
@@ -0,0 +1,48 @@
+##  name.rb
+
+## Exercise 1
+# puts "Please type your name."
+# name = gets.chomp
+# puts "Hello " + name + ". It's nice to meet you."
+
+## Exercise 3
+# puts "Please type your name."
+# name = gets.chomp
+# 10.times do 
+#   puts name
+# end
+
+## Exercise 4
+# puts "What is your first name?"
+# first_name = gets.chomp
+# puts "What is your last name?"
+# last_name = gets.chomp
+# puts "Your full name is " + first_name + " " + last_name
+
+
+## age.rb -- assumes the user will enter an integer
+
+## Exercise 2
+# puts "How old are you?"
+# age = gets.chomp.to_i   
+# puts "In 10 years you will be:" 
+# puts age + 10
+# puts "In 20 years you will be:"
+# puts age + 20
+# puts "In 30 years you will be:"
+# puts age + 30
+# puts "In 40 years you will be:"
+# puts age + 40
+
+
+## Exercise 5
+
+# The first program will print '3' for the value of x, but the second one will
+# throw an error because the variable x is contained within a 'do/end' block. 
+
+## Exercise 6
+
+# This error tells you that you are trying to use a variable "shoes", outside of 
+# it's scope (perhaps it's contained within a loop or block)
+
+
