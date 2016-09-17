8b5e684693661bff45ae9baf7e5446de6e31a1c2
Melissa Kaspshak <mj2tiger@gmail.com>
2016-09-17 23:18:41 +0000

    Exercises from the Methods section

---
+++ methods.rb
@@ -0,0 +1,52 @@
+## Exercise 1  
+
+# def greeting(name)
+#   'Hello, ' + name + '! How are you today?'
+# end
+
+# puts greeting('Bubba')
+
+## Exercise 2
+
+# 1. x = 2 returns 2
+# 2. puts x = 2   returns nil
+# 3. p name = 'Joe'   returns 'Joe'
+# 4. four = 'four'  returns a string 'four'
+# 5. print something = 'nothing'  'nothing' returns nil
+
+## Exercise 3 
+
+# def multiply(number1,number2)
+#   number1 * number2 
+# end
+
+# puts multiply(7,8)
+
+## Exercise 4 
+
+# def scream(words)
+#   words = words + '!!!!!'
+#   return  #  <==== It prints nothing because the return command exits the method
+#   puts words     
+# end
+
+# scream('Yippeee')
+
+## Exercise 5
+
+# part 1
+# def scream(words)
+#   puts words = words + '!!!!!!'
+# end
+
+# scream('Yippeee')
+
+# part 2 It returns Nil
+
+## Exercise 6
+
+# The error message tells you that you've entered the wrong number of arguments.
+# You've only entered one argument and you need two. 
+
+
+
